5773cc481e184dc1c12685265fd86437ea8b1915
tjchxy <37538978+tjchxy@users.noreply.github.com>
2018-07-17 11:47:09 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
 import collections
 import random
-
+import json,os
 import numpy as np
 
 
@@ -25,37 +26,30 @@ def index_data(sentences, dictionary):
 
     return index.reshape(shape)
 
-#vocalbulary 是read_data返回的data
+
 def get_train_data(vocabulary, batch_size, num_steps):
     ##################
     # Your Code here
     ##################
-    data,count,dictionary,reversed_dictionary=build_dataset(vocalbulary,5000)
-    drop=len(data)%batch_size
-    xraw = data[:-drop].reshape(batch_size,)
-    yraw = data[1:-drop+1].reshape(batch_size,)
-    
-    epoch=len(data)//batch_size//num_steps
-    for i in range(epoch):
-        batch = xraw[:,i*num_steps:i*num_steps(i+1)]
-        labels = yraw[:,i*num_steps:i*num_steps(i+1)]
-    return batch,labels
-    
-#words是data，n_words是len（data）
-def build_dataset(words, n_words):
-    """Process raw inputs into a dataset."""
-    count = [['UNK', -1]]
-    count.extend(collections.Counter(words).most_common(n_words - 1))
-    dictionary = dict()
-    for word, _ in count:
-        dictionary[word] = len(dictionary)
-    data = list()
-    unk_count = 0
-    for word in words:
-        index = dictionary.get(word, 0)
-        if index == 0:  # dictionary['UNK']
-            unk_count += 1
-        data.append(index)
-    count[0][1] = unk_count
-    reversed_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
-    return data, count, dictionary, reversed_dictionary
+    data_length = len(vocabulary)
+    with open('dictionary.json','r') as inf:
+        dictionary = json.load(inf)
+        dictionary = json.loads(dictionary)
+
+    raw_x = [dictionary.get(w, 0) for w in vocabulary]
+    raw_y = [dictionary.get(w, 0) for w in vocabulary[1:]]
+    raw_y.append(0)
+
+    batch_partition_length = data_length // batch_size
+    data_x = np.zeros([batch_size, batch_partition_length], dtype=np.int32)
+    data_y = np.zeros([batch_size, batch_partition_length], dtype=np.int32)
+
+    for i in range(batch_size):
+        data_x[i] = raw_x[batch_partition_length * i: batch_partition_length * (i + 1)]
+        data_y[i] = raw_y[batch_partition_length * i: batch_partition_length * (i + 1)]
+
+    epoch_size = batch_partition_length // num_steps
+    for i in range(epoch_size):
+        x = data_x[:, num_steps * i: num_steps * (i + 1)]
+        y = data_y[:, num_steps * i: num_steps * (i + 1)]
+        yield (x, y)
